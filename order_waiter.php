86b65c20e276f833469a2fb4b8d24644ada69578
Andrian Baros <andrianbaros10@gmail.com>
2024-08-11 01:07:17 +0700

    Update order_waiter.php

--- order_waiter.php
+++ order_waiter.php
@@ -1,9 +1,9 @@
 <?php
 // Koneksi ke database
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "resto_sate";
+$username = "wheramye_unikom";
+$password = "YYRJ%5.]QD#Z";
+$dbname = "wheramye_resto_sate";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
